e3631ba48a94999fd5652fbc7d1c63b01c1dd6af
Adam Gerecke <adamgerecke@quickenloans.com>
2020-07-27 16:59:41 -0400

    Added Comments to Code. Removed un-needed spaces making code shorter.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Lab_5._1_RPG
 {
-    class GameCharacter
+    class GameCharacter // base calss to hold info every playable class will need. Intelligence, Strength, and Name
     {
         public string name { get; set; }
         public int strength { get; set; }
@@ -18,12 +18,11 @@ namespace Lab_5._1_RPG
 
         public virtual void Play()
         {
-            Console.WriteLine($"{name} (Intelligence {intelligence}, Strength {strength})");
-            
+            Console.WriteLine($"{name} (Intelligence {intelligence}, Strength {strength})"); // Displays base stats for all classes created.         
         }
     }
 
-    class Warrior : GameCharacter
+    class Warrior : GameCharacter // Warrior Class, has Base Stats, in addiition to a weapon of choice.
     {
         public string weapon { get; set; }
 
@@ -31,51 +30,43 @@ namespace Lab_5._1_RPG
         {
             weapon = theWeapon;
         }
-
         public override void Play()
         {
             base.Play();
-            Console.WriteLine($"{name} uses a {weapon}, as their weapon of choice.");
+            Console.WriteLine($"{name} uses a {weapon}, as their weapon of choice."); // After adding the base stats message, adds a weapon of choice for the hero.
             Console.WriteLine();
         }
     }
 
-    class MagicUsingCharacter : GameCharacter
+    class MagicUsingCharacter : GameCharacter // Defines a Magic Using Character, with base stats, in addition to Magical Energy Stat.
     {
         public int magicalEnergy { get; set; }
         public MagicUsingCharacter(string theName, int theStrength, int theIntelligence, int theMagicalEnergy) : base(theName, theStrength, theIntelligence)
         {
             magicalEnergy = theMagicalEnergy;
         }
-
         public override void Play()
         {
             base.Play();
-            Console.WriteLine($"In addition on their base stats {name}, has a pool of {magicalEnergy} magical energy.");
+            Console.WriteLine($"In addition on their base stats {name}, has a pool of {magicalEnergy} magical energy."); // base message + amount of magical energy
             
         }
-
-
     }
 
 
-    class Wizard : MagicUsingCharacter
+    class Wizard : MagicUsingCharacter // Defines Wizard Class, which takes all of the Stats the Magic Using Class has, in addition to a number of Magical Spells.
     {
         public int spellNumber { get; set; }
-
         public Wizard(string theName, int theStrength, int theIntelligence, int theMagicalEnergy, int theSpellNumber) : base(theName, theStrength, theIntelligence, theMagicalEnergy)
         {
             spellNumber = theSpellNumber;
         }
-
         public override void Play()
         {
             base.Play();
-            Console.WriteLine($"{name} is also able to cast {spellNumber} spells.");
+            Console.WriteLine($"{name} is also able to cast {spellNumber} spells."); // base magic user message with stats and energy, in addition to spells known.
             Console.WriteLine();
         }
-
-
     }
 
 
@@ -86,28 +77,27 @@ namespace Lab_5._1_RPG
             Console.WriteLine("Welcome to World of Dev.BuildCraft.");
             Console.WriteLine();
 
-            List<GameCharacter> gameCharacters = new List<GameCharacter>();
+            List<GameCharacter> character = new List<GameCharacter>(); // new list character based off of the GameCharacter Class
 
-            gameCharacters.Add(new Warrior("Warin", 18, 9, "Battle Axe"));
-            gameCharacters.Add(new Warrior("Oxnar", 20, 6, "Flail"));
-            gameCharacters.Add(new Warrior("Kong", 25, 12, "Great Sword"));
-            gameCharacters.Add(new Warrior("Sigismund", 16, 12, "Battle Staff"));
-            gameCharacters.Add(new Warrior("Warden", 20, 9, "Glaive"));
-            gameCharacters.Add(new Warrior("La Crox", 16, 14, "Tribuchet"));
+            character.Add(new Warrior("Warin", 18, 9, "Battle Axe"));    //Name|Strength|Intelligence|Weapon of Choice
+            character.Add(new Warrior("Oxnar", 20, 6, "Flail"));
+            character.Add(new Warrior("Kong", 25, 12, "Great Sword"));
+            character.Add(new Warrior("Sigismund", 16, 12, "Battle Staff"));
+            character.Add(new Warrior("Warden", 20, 9, "Glaive"));
+            character.Add(new Warrior("La Crox", 16, 14, "Tribuchet"));
 
-            gameCharacters.Add(new Wizard("Elizia", 9, 25, 50, 10));
-            gameCharacters.Add(new Wizard("Rineheart", 6, 27, 54, 8));
-            gameCharacters.Add(new Wizard("Dalenor", 12, 20, 40, 14));
-            gameCharacters.Add(new Wizard("Edali", 14, 18, 36, 19));
-            gameCharacters.Add(new Wizard("Ekonn", 17, 15, 30, 6));
-            gameCharacters.Add(new Wizard("Sigoris", 17, 12, 24, 3));
+            character.Add(new Wizard("Elizia", 9, 25, 50, 10));        //Name|Strength|Intelligence|Magical Energy|Known Spell Count
+            character.Add(new Wizard("Rineheart", 6, 27, 54, 8));
+            character.Add(new Wizard("Dalenor", 12, 20, 40, 14));
+            character.Add(new Wizard("Edali", 14, 18, 36, 19));
+            character.Add(new Wizard("Ekonn", 17, 15, 30, 6));
+            character.Add(new Wizard("Sigoris", 17, 12, 24, 3));
 
 
-            foreach(GameCharacter CharacterClass in gameCharacters)
+            foreach(GameCharacter CharacterClass in character)  // for each entry of the class GameCharacter run the Play Method on the CharacterClass Variable of character.
             {
                 CharacterClass.Play();
             }
-
         }
     }
 }
